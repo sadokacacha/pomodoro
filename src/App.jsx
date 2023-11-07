c0800e6fd8d1cf652fa1823ce1ceb4c37ec290c2
sadokacacha <sadokacacha1998@gmail.com>
2023-11-07 12:06:13 +0100

    fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,79 @@ import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const [seconds, setSeconds] = useState(60);
-  const [minute, setMinutes] = useState(24);
-  const [pause, setPause] = useState(false);
+  const [seconds, setSeconds] = useState(0);
+  const [minute, setMinutes] = useState(25);
+  const [start, setstart] = useState(true);
 
   let intervalRef = useRef();
 
   const decreaseSeconds = () => setSeconds((prev) => prev - 1);
-  useEffect(() => {
-    intervalRef.current = setInterval(decreaseSeconds, 1000);
-
-    return () => clearInterval(intervalRef.current);
-  }, []);
+  const decreaseMinute = () => {
+    setMinutes((prev) => prev - 1);
+    setSeconds(59);
+  };
+  const startCounter = () => setstart((start) => !start);
+  /* the pause func */
 
   const handleClick = () => {
-    if (!pause) {
-      clearInterval(intervalRef.current);
-    } else {
+    if (!start) {
       intervalRef.current = setInterval(decreaseSeconds, 1000);
+    } else {
+      clearInterval(intervalRef.current);
     }
-    setPause((prev) => !prev);
+    startCounter();
   };
+
+  /* the main counter  func */
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      if (seconds > 0) {
+        decreaseSeconds();
+      } else {
+        if (minute > 0) {
+          decreaseMinute();
+        }
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalRef.current);
+  }, [seconds, minute]);
+
+  /* the alert one idk why i added this */
+
+  if (seconds === 0 && minute === 0) {
+    alert('times up desu');
+  }
 
   return (
     <div className="pomodoro_app">
       <div className="top_bar">
         <h1> pomodoro </h1>
-        <hr />
+        <hr className="title_line" />
       </div>
-<div className="pomo_container">
+      <div className="pomo_container">
+        <div className="pomo_Count">
+          <div className="pomodoro_type">
+            <ul>
+              <li> pomodoro </li>
+              <li>break</li>
+              <li>long break</li>
+            </ul>
+          </div>
 
-   <div className="pomo_Count">
-        <div className="pomodoro_type">
-          <ul>
-            <li> pomodoro </li>
-            <li>break</li>
-            <li>long break</li>
-          </ul>
-        </div>
+          <span className="timer_countdown">
+            {' '}
+            <p>{`${minute.toString().padStart(2, '0')}:${seconds
+              .toString()
+              .padStart(2, '0')}`}</p>
+          </span>
 
-        <span className="timer_countdown">
-          {' '}
-          {minute} : {seconds}{' '}
-        </span>
-
-        <div className='btn'>
-          <button onClick={handleClick}>{pause ? 'start' : 'pause'}</button>
+          <div className="btn">
+            <button onClick={handleClick}>{start ? 'start' : 'pause'}</button>
+          </div>
         </div>
       </div>
-</div>
-     
     </div>
   );
 }
