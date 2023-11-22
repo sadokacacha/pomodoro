893349c3445e5a1dd7d030fb8ce8491d632c83d7
sadokacacha <sadokacacha1998@gmail.com>
2023-11-22 20:43:51 +0100

    added some code still stuck tho

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   const [seconds, setSeconds] = useState(0);
   const [minute, setMinutes] = useState(25);
+  const [btnClick, setBtnClick] = useState('InactivelistBtn');
+  const [pomodoroBtnClick, setPomodoroBtnClick] = useState(true);
+  const [shortBreakBtnClick, setShortBreakBtnClick] = useState(false);
+  const [longBreakBtnClick, setLongBreakBtnClick] = useState(false);
   let [start, setStart] = useState(false);
 
   let intervalRef = useRef();
@@ -19,24 +23,34 @@ function App() {
     setStart((start) => !start);
   };
 
-
- const handlePomodoro = () => {
-   setMinutes(25);
-   setSeconds(0);
-   setStart(false);
- };
-
+  const handlePomodoro = () => {
+    setMinutes(25);
+    setSeconds(0);
+    setStart(false);
+    setBtnClick('ActiveListBtn');
+    setPomodoroBtnClick(true);
+    setShortBreakBtnClick(false);
+    setLongBreakBtnClick(false);
+  };
 
   const handleShortBreakClick = () => {
     setMinutes(5);
     setSeconds(0);
     setStart(false);
+    setBtnClick('ActiveListBtn');
+    setPomodoroBtnClick(false);
+    setShortBreakBtnClick(true);
+    setLongBreakBtnClick(false);
   };
 
   const handleLongBreakClick = () => {
     setMinutes(15);
     setSeconds(0);
     setStart(false);
+    setBtnClick('ActiveListBtn');
+    setPomodoroBtnClick(false);
+    setShortBreakBtnClick(false);
+    setLongBreakBtnClick(true);
   };
 
   /* the main counter  func */
@@ -57,7 +71,7 @@ function App() {
     return () => clearInterval(intervalRef.current);
   }, [seconds, minute, start]);
 
-  /* the alert one idk why i added this  ,   !! must replay this with  a sound effect when the counter is done !! */
+  /* the alert one idk why i added this  ,   !! must replace this with  a sound effect when the counter is done !! */
 
   if (seconds === 0 && minute === 0) {
     alert('times up desu');
@@ -72,12 +86,27 @@ function App() {
       <div className="pomo_container">
         <div className="pomo_Count">
           <div className="pomodoro_type">
-            <button className="" onClick={handlePomodoro} >pomodoro </button>
-            <button className=" short break" onClick={handleShortBreakClick}>
+            <button
+              className={pomodoroBtnClick ? 'ActiveListBtn' : 'InactivelistBtn'}
+              onClick={handlePomodoro}
+            >
+              pomodoro{' '}
+            </button>
+            <button
+              className={
+                shortBreakBtnClick ? 'ActiveListBtn' : 'InactivelistBtn'
+              }
+              onClick={handleShortBreakClick}
+            >
               {' '}
               short break
             </button>
-            <button className=" long break" onClick={handleLongBreakClick}>
+            <button
+              className={
+                longBreakBtnClick ? 'ActiveListBtn' : 'InactivelistBtn'
+              }
+              onClick={handleLongBreakClick}
+            >
               long break
             </button>
           </div>
